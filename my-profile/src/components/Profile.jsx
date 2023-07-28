e4bd98765a96e34948f71d1046b9d3798d575850
Mister-Friday <i.moxoff@yandex.ru>
2023-07-28 18:14:36 +0300

    feat: add cool profile image

---
+++ my-profile/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfilePicture from "./ProfilePicture";
+
+function Profile() {
+  return (
+    <>
+      <ProfilePicture />
+    </>
+  );
+}
+
+export default Profile;
---
+++ my-profile/src/components/ProfilePicture.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProfilePicture() {
+  return (
+    <img src="https://avatars.githubusercontent.com/u/125909673?v=4"></img>
+  );
+}
+
+export default ProfilePicture;
